55578a2ea8e95b13e216b25fbabdf06c451e376b
EungJun Kim <105894868+dmdwns2@users.noreply.github.com>
2023-09-14 12:26:11 +0900

    #12_Clean_Architectrue (#17)
    * #9 create_model
    model간 의존성 제거
    lombok, persistence에 의존하던 import 전부 삭제
    
    * #9 create_model
    Entity와 헷갈릴 수 있으므로 model로 변경
    
    * [DEV] Create Usecase Annotation
    
    * [REFACTOR] Separate Modules
    - 첫 줄 공백 제거
    - 엔티티에서 영속성 의존을 제거하면서 data 모듈로 이동
    - 모듈 분리에 따른 데이터 접근 인터페이스 추가
    
    * [Refactor] Module Removal
    - data -> persistence 이름 변경
    - application 모듈 추가
    - application이  persistence 모듈에 의존성 추가
    
    * [Refactor] Module Removal
    - application 이 domain, peresistence에 의존성 추가
    
    * [Refactor] Module Removal
    - presentation 모듈에 application 모듈 의존성 추가
    
    * [Refactor] Module Removal
    - jakarta 관련 호환 문제 해결
    - dependency 최적화
    - init.sql 은 수정 예정
    - JPAEntity에 final 제거 ( foreign key 관련 에러)
    
    * [DEV] Create Schema
    
    * Update README
    
    * [DEV] Add BeanConfig

--- persistence/docker/init.sql
+++ persistence/docker/init.sql
@@ -1,1 +1,41 @@
 CREATE DATABASE chatting;
+
+CREATE TABLE `chatting`.`chatroom` (
+  `chatroom_id` bigint NOT NULL AUTO_INCREMENT,
+  `owner` varchar(255) DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`chatroom_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `chatting`.`user` (
+  `user_id` bigint NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `nickname` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `UK_gj2fy3dcix7ph7k8684gka40c` (`name`),
+  UNIQUE KEY `UK_n4swgcf30j6bmtb4l4cjryuym` (`nickname`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `chatting`.`chatting` (
+  `chatroom_id` bigint DEFAULT NULL,
+  `chatting_id` bigint NOT NULL AUTO_INCREMENT,
+  `user_id` bigint DEFAULT NULL,
+  `message` text,
+  PRIMARY KEY (`chatting_id`),
+  KEY `FK9egqjagsko4p1uidnfqol6bta` (`chatroom_id`),
+  KEY `FKq6nviowmr99ttn5pcxy1xcrrt` (`user_id`),
+  CONSTRAINT `FK9egqjagsko4p1uidnfqol6bta` FOREIGN KEY (`chatroom_id`) REFERENCES `chatroom` (`chatroom_id`),
+  CONSTRAINT `FKq6nviowmr99ttn5pcxy1xcrrt` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `chatting`.`user_chatroom` (
+  `chatroom_id` bigint DEFAULT NULL,
+  `user_chatroom_id` bigint NOT NULL AUTO_INCREMENT,
+  `user_id` bigint DEFAULT NULL,
+  PRIMARY KEY (`user_chatroom_id`),
+  KEY `FKhrufhecf4lthmybck55qddra7` (`chatroom_id`),
+  KEY `FK9if7htlurn1a8m287hmla3y6k` (`user_id`),
+  CONSTRAINT `FK9if7htlurn1a8m287hmla3y6k` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`),
+  CONSTRAINT `FKhrufhecf4lthmybck55qddra7` FOREIGN KEY (`chatroom_id`) REFERENCES `chatroom` (`chatroom_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
