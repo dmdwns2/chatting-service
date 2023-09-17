59af31ef13578a1a36d17f1dfe74a0d6a14fc1e8
EungJun Kim <105894868+dmdwns2@users.noreply.github.com>
2023-09-17 18:18:36 +0900

    [Test] Add tests for SignUpUseCaseImpl (#21)
    - SignUpUseCaseImpl에 대한 테스트 추가

--- application/src/test/java/com/example/usecase/impl/SignUpUseCaseImplTest.java
+++ application/src/test/java/com/example/usecase/impl/SignUpUseCaseImplTest.java
@@ -1,18 +1,22 @@
 package com.example.usecase.impl;
 
+import com.example.dto.UserCreatedEvent;
 import com.example.exception.DuplicateNameException;
 import com.example.exception.DuplicateNicknameException;
 import com.example.port.CurrentDataTimePort;
 import com.example.port.ExistsNamePort;
 import com.example.port.ExistsNicknamePort;
 import com.example.port.SaveUserPort;
-import com.example.value.SignUpCommand;
+import com.example.dto.SignUpCommand;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.transaction.support.TransactionTemplate;
 
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -41,21 +45,27 @@ public class SignUpUseCaseImplTest {
                 existsNamePort,
                 existsNicknamePort,
                 saveUserPort,
-                currentDataTimePort,
-                mock(TransactionTemplate.class)
+                currentDataTimePort
         );
     }
 
     @Test
     void join_ValidUser_ReturnsUserCreatedEvent() {
-        //TODO
+        SignUpCommand command = new SignUpCommand("messi", "2022wc", "leo");
+        when(existsNamePort.existsName(command.getName())).thenReturn(false);
+        when(existsNicknamePort.existsNickname(command.getNickname())).thenReturn(false);
+        when(currentDataTimePort.now()).thenReturn(LocalDateTime.now());
 
+        UserCreatedEvent result = signUpUseCase.join(command);
+
+        assertNotNull(result);
+        Assertions.assertThat(command.getName()).isEqualTo(result.getName());
     }
 
 
     @Test
     void join_DuplicateName_ThrowsDuplicateNameException() {
-        SignUpCommand command = new SignUpCommand("existingName", "password", "nickname");
+        SignUpCommand command = new SignUpCommand("messi", "2022wc", "leo");
         when(existsNamePort.existsName(command.getName())).thenReturn(true);
 
         assertThrows(DuplicateNameException.class, () -> signUpUseCase.join(command));
@@ -63,7 +73,7 @@ public class SignUpUseCaseImplTest {
 
     @Test
     void join_DuplicateNickname_ThrowsDuplicateNicknameException() {
-        SignUpCommand command = new SignUpCommand("username", "password", "existingNickname");
+        SignUpCommand command = new SignUpCommand("messi", "2022wc", "leo");
         when(existsNicknamePort.existsNickname(command.getNickname())).thenReturn(true);
 
         assertThrows(DuplicateNicknameException.class, () -> signUpUseCase.join(command));
