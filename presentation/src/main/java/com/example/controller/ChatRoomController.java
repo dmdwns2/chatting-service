6eacc3a30de22fe8ba580022e61d6f00b82d4e0d
dmdwns2 <ssreki1@naver.com>
2024-01-26 18:00:20 +0900

    feat: chatroom controller - join chatroom function - add view

--- presentation/src/main/java/com/example/controller/ChatRoomController.java
+++ presentation/src/main/java/com/example/controller/ChatRoomController.java
@@ -8,10 +8,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequiredArgsConstructor
@@ -36,5 +33,10 @@ public class ChatRoomController {
         return "chatroomlist";
     }
 
-
+    @PostMapping("/rooms/join/{owner}")
+    public String join(@PathVariable String owner, Authentication authentication) {
+        String name = authentication.getName();
+        chatRoomService.join(owner, name);
+        return "home"; // TODO : home -> chatroom
+    }
 }
