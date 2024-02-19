2c3e9f38a35f3bc3b805a863798c55144736c857
dmdwns2 <ssreki1@naver.com>
2024-02-19 13:00:36 +0900

    build: add websocket dependency

--- persistence/docker/init.sql
+++ persistence/docker/init.sql
@@ -2,7 +2,7 @@ CREATE DATABASE chatting;
 
 CREATE TABLE `chatting`.`chatroom` (
   `chatroom_id` bigint NOT NULL AUTO_INCREMENT,
-  `owner` varchar(255) DEFAULT NULL,
+  `owner` bigint NOT NULL,
   `title` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`chatroom_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -18,12 +18,12 @@ CREATE TABLE `chatting`.`user` (
   UNIQUE KEY `UK_n4swgcf30j6bmtb4l4cjryuym` (`nickname`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-CREATE TABLE `chatting`.`chatting` (
+CREATE TABLE `chatting`.`chatmsg` (
   `chatroom_id` bigint DEFAULT NULL,
-  `chatting_id` bigint NOT NULL AUTO_INCREMENT,
+  `chatmsg_id` bigint NOT NULL AUTO_INCREMENT,
   `user_id` bigint DEFAULT NULL,
   `message` text,
-  PRIMARY KEY (`chatting_id`),
+  PRIMARY KEY (`chatmsg_id`),
   KEY `FK9egqjagsko4p1uidnfqol6bta` (`chatroom_id`),
   KEY `FKq6nviowmr99ttn5pcxy1xcrrt` (`user_id`),
   CONSTRAINT `FK9egqjagsko4p1uidnfqol6bta` FOREIGN KEY (`chatroom_id`) REFERENCES `chatroom` (`chatroom_id`),
