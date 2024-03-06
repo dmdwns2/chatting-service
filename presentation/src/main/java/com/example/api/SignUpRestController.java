10845d45f2dd82c74344c3fd43d8c5b2ee52f837
dmdwns2 <ssreki1@naver.com>
2024-03-06 18:26:20 +0900

    chore: fix signup rest controller

--- presentation/src/main/java/com/example/api/SignUpRestController.java
+++ presentation/src/main/java/com/example/api/SignUpRestController.java
@@ -9,6 +9,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,7 +22,7 @@ public class SignUpRestController {
     private final SignUpUseCase signUpUseCase;
 
     @PostMapping("/signup")
-    public Response<String> signup(@Validated SignupForm signupForm) {
+    public Response<String> signup(@RequestBody @Validated SignupForm signupForm) {
         UserCreatedEvent userCreatedEvent = signUpUseCase.join(new SignUpCommand(
                 signupForm.getName(),
                 signupForm.getPassword(),
@@ -29,6 +30,6 @@ public class SignUpRestController {
         ));
 
         return Response.success("login complete. name : " + userCreatedEvent.getName()
-                + " time : " + userCreatedEvent.getCreatedAt());
+                + " time : " + userCreatedEvent.getCreatedAt().toString());
     }
 }
